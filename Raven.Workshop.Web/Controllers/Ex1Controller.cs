0a25a0cf5c74bdf11c67a86c385cd3e7c5da3806
Pawel Pekrol <ppekrol@ais.pl>
2013-12-11 21:33:17 +0100

    sample controller

---
+++ Raven.Workshop.Web/Controllers/Ex1Controller.cs
@@ -0,0 +1,25 @@
+﻿namespace Raven.Workshop.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Raven.Workshop.Web.Models;
+
+    public class Ex1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Raven.Workshop.Web/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Raven.Workshop.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TestModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
