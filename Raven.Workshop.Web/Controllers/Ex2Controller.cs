0d3d3bc1ad49dbe135329fa65a19fec30276c6b8
Arkadiusz Palinski <arek@ais.pl>
2013-12-14 20:04:50 +0100

    removing file headers

--- Raven.Workshop.Web/Controllers/Ex2Controller.cs
+++ Raven.Workshop.Web/Controllers/Ex2Controller.cs
@@ -1,10 +1,4 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="Ex2Controller.cs" company="Hibernating Rhinos LTD">
-//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
-
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Raven.Workshop.Web.Models;
 
--- Raven.Workshop.Web/Models/Company.cs
+++ Raven.Workshop.Web/Models/Company.cs
@@ -1,10 +1,4 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="Company.cs" company="Hibernating Rhinos LTD">
-//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
-
-namespace Raven.Workshop.Web.Models
+﻿namespace Raven.Workshop.Web.Models
 {
     using System.Collections.Generic;
 
